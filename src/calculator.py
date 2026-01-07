cbc463d66f04c6a9208b8a7ef47a1ba4f8ec0acf
emmasvd2 <emma.stievenard04@gmail.com>
2026-01-07 11:52:56 +0100

    Ajout fonction min et tests associés

--- src/calculator.py
+++ src/calculator.py
@@ -2,3 +2,7 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
+    
+
+    def min(self, a, b):
+        return a if a < b else b
